52b3ddf85a98e30ed513bfb1c9fd811ab173a31a
aissam-out <39061689+aissam-out@users.noreply.github.com>
2020-12-17 10:23:23 +0100

    commenting flask upload function

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 # if you encounter dependency issues using 'pip install flask-uploads'
-# try 'pip install Flask-Reuploaded' 
+# try 'pip install Flask-Reuploaded'
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from keras.preprocessing.image import load_img
 # the pretrained model
@@ -20,20 +20,24 @@ def home():
     welcome = "Hello, World !"
     return welcome
 
+# the main route for upload and prediction
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
+        # save the image
         filename = photos.save(request.files['photo'])
-
+        # load the image
         image = load_img('./static/img/'+filename, target_size=(224, 224))
+        # process the image
         image = process_image(image)
+        # make prediction
         prediction, percentage = predict_class(image)
-
+        # the answer which will be rendered back to the user
         answer = "For {} : <br>The prediction is : {} <br>With probability = {}".format(filename, prediction, percentage)
         return answer
+    # web page to show before the POST request containing the image
     return render_template('upload.html')
 
 
 if __name__ == '__main__':
-	#app.run(debug=True)
     app.run(host='0.0.0.0', debug=True)
